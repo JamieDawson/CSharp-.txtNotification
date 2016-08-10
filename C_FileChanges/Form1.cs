f69161a56b4486b6584b1032c2959820a244efe8
Jamie Robert Dawson <jamierobertdawson@gmail.com>
2016-08-10 14:38:10 -0400

    Run this program and update a txt file on your computer.

---
+++ C_FileChanges/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+//A MessageBox will appaer when you change a text file. This works on all text files.
+
+namespace C_FileChanges
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            FileSystemWatcher fsw = new FileSystemWatcher();
+            fsw.Path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            fsw.NotifyFilter = NotifyFilters.LastWrite;
+            fsw.Filter = "*.txt";
+            fsw.Changed += Fsw_Changed;
+            fsw.EnableRaisingEvents = true;
+        }
+
+        private void Fsw_Changed(object sender, FileSystemEventArgs e)
+        {
+            MessageBox.Show("You have saved a text file.");
+        }
+    }
+}
